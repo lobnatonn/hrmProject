222569af20cd00a5d00641e8bb5e6772936c8483
lobnatonn <lobnatonn@esprit.tn>
2013-09-25 10:48:39 +0200

    first project
    Signed-off-by: lobnatonn <lobnatonn@esprit.tn>

---
+++ tn.edu.esprit.erpBi.hrmProject/ejbModule/tn/edu/esprit/erpBi/hrmProject/domain/Employee.java
@@ -0,0 +1,39 @@
+package tn.edu.esprit.erpBi.hrmProject.domain;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Employee
+ *
+ */
+@Entity
+
+public class Employee implements Serializable {
+
+	
+	private int idEmployee;
+	private String nameEmployee;
+	private static final long serialVersionUID = 1L;
+
+	public Employee() {
+		super();
+	}   
+	@Id    
+	public int getIdEmployee() {
+		return this.idEmployee;
+	}
+
+	public void setIdEmployee(int idEmployee) {
+		this.idEmployee = idEmployee;
+	}   
+	public String getNameEmployee() {
+		return this.nameEmployee;
+	}
+
+	public void setNameEmployee(String nameEmployee) {
+		this.nameEmployee = nameEmployee;
+	}
+   
+}
