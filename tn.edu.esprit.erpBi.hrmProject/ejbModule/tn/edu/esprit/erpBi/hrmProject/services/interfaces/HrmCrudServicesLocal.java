83df22e80ce41d1d0ef125dbf8a2f63b1efab418
lobnatonn <lobnatonn@esprit.tn>
2013-09-25 12:37:03 +0200

    the implementation of the CRUD of the entity Employee
    ;)
    
    Signed-off-by: lobnatonn <lobnatonn@esprit.tn>

---
+++ tn.edu.esprit.erpBi.hrmProject/ejbModule/tn/edu/esprit/erpBi/hrmProject/services/interfaces/HrmCrudServicesLocal.java
@@ -0,0 +1,8 @@
+package tn.edu.esprit.erpBi.hrmProject.services.interfaces;
+
+import javax.ejb.Local;
+
+@Local
+public interface HrmCrudServicesLocal {
+
+}
---
+++ tn.edu.esprit.erpBi.hrmProject/ejbModule/tn/edu/esprit/erpBi/hrmProject/services/interfaces/HrmCrudServicesRemote.java
@@ -0,0 +1,20 @@
+package tn.edu.esprit.erpBi.hrmProject.services.interfaces;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import tn.edu.esprit.erpBi.hrmProject.domain.Employee;
+
+@Remote
+public interface HrmCrudServicesRemote {
+	public void addEmployee(Employee employee);
+
+	public Employee findEmployeeById(int idEmployee);
+
+	public void updateEmployee(Employee employee);
+
+	public void deleteEmployee(int idEmployee);
+
+	public List<Employee> findAllEmployees();
+}
